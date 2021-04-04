05a549f964a4877ebf129f28b51da27e08d82813
Salvador Vittor <61999704+NahVih@users.noreply.github.com>
2021-04-04 21:21:56 +1000

    Update Flow.java

--- src/Flow.java
+++ src/Flow.java
@@ -12,13 +12,13 @@ public class Flow {
         System.out.println("Fim do main");
     }
 
-    private static void metodo1() {
+    private static void metodo1() throws MyExcepcion {
         System.out.println("Ini do metodo1");
         metodo2();
         System.out.println("Fim do metodo1");
     }
 
-    private static void metodo2() {
+    private static void metodo2() throws MyExcepcion {
         System.out.println("Ini do metodo2");
         throw new MyExcepcion ("System is not working");
         //System.out.println("Fim do metodo2");
