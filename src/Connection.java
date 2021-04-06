6c39c92061df149bdc32a3b5c16eb5973c4b679a
Salvador Vittor <61999704+NahVih@users.noreply.github.com>
2021-04-06 16:41:30 +1000

    Add files via upload

---
+++ src/Connection.java
@@ -0,0 +1,15 @@
+public class Connection {
+
+	public Connection() {
+		System.out.println("Creating Connection");
+	}
+	
+	public void readData() {
+		System.out.println("Receiving Data");
+		//throw new IllegalSelectorException();		
+	}
+	
+	public void End() {
+		System.out.println("Closing Coneection");
+	}
+}
